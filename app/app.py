a62ed6c69f0c630fc74a37db343db510d5396c4a
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2021-08-29 20:04:39 -0500

    CODE_TO_DIAGRAM TRANSFORMATION && SOME UPDATES
    Python code to graph transformation was majority completed. This was completed by using VisJS Library (that is also uploaded on this commit).

--- app/app.py
+++ app/app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, redirect
+from re import sub
+from flask import Flask, render_template, redirect, request     #Flask import
+import ast                                                      #Abstract Syntax Tree import
+import json                                                     #Import Json for send info to JS
 
 app = Flask(__name__)
 
@@ -23,14 +26,82 @@ def transformaciones():
     return render_template('transformaciones.html')
 
 #Ruta hacia la página de transformaciones código-digrama
-@app.route('/code_to_diagram')
+@app.route('/code_to_diagram',  methods = ['GET', 'POST'])
 def code_to_diagram():
-    return render_template('code_to_diagram.html')
+    
+    outputDictionary = {}
+
+    #Recieve python code File
+    if request.method == 'POST':
+
+        python_file = request.files['archivoPython']
+
+        code = python_file.read()
+
+        #Convert recieved python file to Python Abstract Syntax Tree
+        tree = ast.parse(code)
+        outputDictionary = extractGraphInfo(tree)
+
+    return render_template('code_to_diagram.html', PythonDic = outputDictionary)
+
+#-------- MÉTODO PARA ANALIZAR EL AST DE PYTHON ---------
+def extractGraphInfo(code_tree): 
+
+    output_dictionary = {
+
+    }
+
+    temp_class_attributes_list = []
+    temp_class_methods_list = []
+
+    #Walk throught the ast 
+    for nodo in code_tree.body:
+
+        #If a class appear
+        if isinstance(nodo, ast.ClassDef): 
+
+            #Search for the Methods in the class
+            for sub_node in nodo.body: 
+
+                if isinstance(sub_node, ast.FunctionDef):
+                    
+                    temp_class_methods_list.append(sub_node.name); 
+
+                    #In the __init__ method, search for the class attributes
+                    if(sub_node.name == '__init__'): 
+
+                        fuction_comps = ast.walk(sub_node)
+
+                        for possibleAttribute in fuction_comps: 
+
+                            if isinstance(possibleAttribute, ast.Attribute): 
+                                
+                                temp_class_attributes_list.append(possibleAttribute.attr)
+                            
+                            else: 
+
+                                pass
+            
+
+            output_dictionary[nodo.name] = {
+
+                'Attributes' : [i for i in temp_class_attributes_list], 
+                'Methods': [i for i in temp_class_methods_list]
+
+            }
+
+            #Clear attributes and methods lists
+            temp_class_methods_list.clear()
+            temp_class_attributes_list.clear()
+    
+    return output_dictionary
+    
 
 #Ruta hacia la página de transformaciones diagrama-código
 @app.route('/diagram_to_code')
 def diagram_to_code():
     return render_template('diagram_to_code.html')
+
 
 if __name__ == "__main__": 
     app.run(debug=True)
